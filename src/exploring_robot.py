f420c04d6a48766755ec9f8359187f0ff20c8287
Alex Cooper <amcooper2@sheffield.ac.uk>
2021-05-03 12:13:35 +0100

    3 zones reached

--- src/exploring_robot.py
+++ src/exploring_robot.py
@@ -16,8 +16,11 @@ class ExploringRobot(object):
     def __init__(self):
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         rospy.init_node('odom_node', anonymous=True)
-        self.rate = rospy.Rate(10) # hz
+        self.rate = rospy.Rate(5) # hz
         self.lidar_subscriber = rospy.Subscriber('/scan', LaserScan, self.callback_lidar)
+        self.object_distance = 1
+        self.distance_right = 1
+        self.distance_left = 1
 
         self.ctrl_c = False
         rospy.on_shutdown(self.shutdownhook)
@@ -30,12 +33,34 @@ class ExploringRobot(object):
         self.ctrl_c = True
 
     def shutdown_function(self):
+        vel_cmd = Twist()
+        vel_cmd.linear.x = 0.0
+        vel_cmd.angular.z = 0.0
+        self.pub.publish(vel_cmd)
         print("stopping publisher node at: {}".format(rospy.get_time()))
 
     def main_loop(self):
+        vel_cmd = Twist()
+        while not self.ctrl_c:
+            if self.object_distance > 0.4:
+                vel_cmd.linear.x = 0.3
+                vel_cmd.angular.z = 0.0
+                print("moving forward")
+                self.pub.publish(vel_cmd)
+            elif self.distance_right > self.distance_left :
+                vel_cmd.linear.x = 0.0
+                vel_cmd.angular.z = -0.5
+                print("turning right")
+                self.pub.publish(vel_cmd)
+            else:
+                vel_cmd.linear.x = 0.0
+                vel_cmd.angular.z = 0.5
+                print("turning left")
+                self.pub.publish(vel_cmd)
+                
         #self.turn_left(3)
         #self.turn_right(3)
-        self.move_forward(3)
+        #self.move_forward(3)
         #self.move_backwards(3)
         #print(object_distance)
         #self.robot_controller = MoveTB3()
@@ -96,12 +121,22 @@ class ExploringRobot(object):
         self.pub.publish(vel_cmd)
 
     def callback_lidar(self, lidar_data):
-        print("test")
-        left_arc = lidar_data.ranges[0:10]
-        right_arc = lidar_data.ranges[-10:]
+        left_arc = lidar_data.ranges[0:50]
+        right_arc = lidar_data.ranges[-50:]
         front_arc = np.array(left_arc + right_arc)
         # find the miniumum object distance within the frontal laserscan arc:
         self.object_distance = front_arc.min()
+
+        left_arc = lidar_data.ranges[20:40]
+        right_arc = lidar_data.ranges[40:60]
+        total_arc1 = np.array(left_arc + right_arc)
+        self.distance_left = total_arc1.min()
+
+        left_arc = lidar_data.ranges[-60:-40]
+        right_arc = lidar_data.ranges[-40:-20]
+        total_arc2 = np.array(left_arc + right_arc)
+        self.distance_right = total_arc2.min()
+        #print(self.object_distance)
 
 if __name__ == '__main__':
     publisher_instance = ExploringRobot()
